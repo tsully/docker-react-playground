677ae326cb5c3e06dd72623bae8f9c7450e7c6f1
Tyler Sullberg <tysullberg@gmail.com>
2020-12-30 14:32:08 -0800

    test commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HOWDY</p>
+        <p>HOWDY: This is a test change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
